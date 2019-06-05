069b478621002926a963b727930956c772218822
henrique.mello <henrique.mello@N279>
2019-06-05 16:17:41 -0300

    Adicionando contexto e configurações da string de conexão

---
+++ Switch.Domain/Entities/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using Switch.Domain.Enums;
+using System;
+
+namespace Switch.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string SobreNome { get; private set; }
+        public string Email { get; private set; }
+        public string Senha { get; private set; }
+        public DateTime DataNascimento { get; private set; }
+        public SexoEnum Sexo { get; private set; }
+        public string UrlFoto { get; private set; }
+    }
+}
