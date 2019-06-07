d27c5d624782a0f345183c58bbd445f397158d03
henrique.mello <henrique.mello@N279>
2019-06-07 17:08:47 -0300

    Console App para testes

--- Switch.Domain/Entities/Usuario.cs
+++ Switch.Domain/Entities/Usuario.cs
@@ -7,10 +7,10 @@ namespace Switch.Domain.Entities
     public class Usuario
     {
         public int Id { get; private set; }
-        public string Nome { get; private set; }
-        public string SobreNome { get; private set; }
+        public string Nome { get;  set; }
+        public string SobreNome { get; set; }
         public string Email { get; private set; }
-        public string Senha { get; private set; }
+        public string Senha { get;  set; }
         public DateTime DataNascimento { get; private set; }
         public SexoEnum Sexo { get; private set; }
         public string UrlFoto { get; private set; }
---
+++ SwitchApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Switch.Domain.Entities;
+using Switch.Infra.Data.Context;
+using System;
+
+namespace SwitchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var usuario = new Usuario() { Nome = "Henrique", SobreNome = "Mello",Senha="12323" };
+            var optionsBuilder = new DbContextOptionsBuilder<SwitchContext>();
+            optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseMySql("Server=localhost;userid=root;password=123456;database=SwitchDB", 
+                m => m.MigrationsAssembly("Switch.Infra.Data"));
+
+
+
+            try
+            {
+                using (var dbContext = new SwitchContext(optionsBuilder.Options))
+                {
+                    dbContext.Usuarios.Add(usuario);
+                    dbContext.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
